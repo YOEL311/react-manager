2b66d32d99d84dd55d8867a65381ac96b0c946c9
Yoel31 <yoel301@gmail.com>
2019-08-19 02:02:44 +0300

    config to git ignore

--- src/App.js
+++ src/App.js
@@ -8,15 +8,6 @@ import Router from "./Router";
 import { config } from "../config";
 export default class App extends Component {
   componentDidMount(): void {
-    const confi2g = {
-      apiKey: "AIzaSyBUY0w1ACTmZejX-sj4zfr-Mp3JGw-Vlt0",
-      authDomain: "react-manager-ee9f5.firebaseapp.com",
-      databaseURL: "https://react-manager-ee9f5.firebaseio.com",
-      projectId: "react-manager-ee9f5",
-      storageBucket: "",
-      messagingSenderId: "451681222031",
-      appId: "1:451681222031:web:1bd830159b3a2dad"
-    };
     // Initialize Firebase
     firebase.initializeApp(config);
   }
